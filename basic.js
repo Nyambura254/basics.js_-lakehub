9f67127002302f70eb4b22de04439bdb5b4ab1f5
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-05-11 18:12:00 +0300

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,23 @@
+//data types
+let myString = "this is a string" //string
+    //provide contex and reference to a storage location...using myString
+var myNum = 100; //number
+let my_array = [2, 'a' [1, 2], { a: 1, b: 2 }] //array
+    {
+        name = "bonnie",
+            age = 24,
+            gender = "male"
+    };
+
+//operators
+
+//arithmentic operators
+7 % 2; //modulo porator (binary),7 and 2 are operands{they operate on two operands}
+
+//logical operators 
+//logical AND
+!false; //! is operator
+//(unary) and false is operand
+
+//truth table
+let T1 = true && true = true;
